ae55a399486e1681dbaf3b5486578f4884db8b73
Samson Svendsen <Svendsen@nordictrustee.com>
2021-08-23 12:12:23 +0200

    Updates

---
+++ precache-manifest.0f99d08f11211f31a00a50d5a0a254bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e9567392023eb424b8bd4e58515d2b",
+    "url": "/milabowl/index.html"
+  },
+  {
+    "revision": "ed5ae30e098c2e193413",
+    "url": "/milabowl/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ca7609978c0a5cdb6d1c",
+    "url": "/milabowl/static/js/2.d39d3ae1.chunk.js"
+  },
+  {
+    "revision": "4e4e8b39ecf9882f4c4281fd5207264d",
+    "url": "/milabowl/static/js/2.d39d3ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed5ae30e098c2e193413",
+    "url": "/milabowl/static/js/main.fd81ab83.chunk.js"
+  },
+  {
+    "revision": "ea87fb01612fc1479137",
+    "url": "/milabowl/static/js/runtime-main.5ebe3426.js"
+  }
+]);
