440f70ec0338e55bd978257444e52ef4f7b26a73
Samson Svendsen <Svendsen@nordictrustee.com>
2022-08-08 10:52:02 +0200

    Updates

---
+++ precache-manifest.d6184f9a61b528f029a0e9abb01bdb44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82f2c531e4a9dacc3a6bf826299b65e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff842c5436b818d55093",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "687b1c195a61c3ed5bc6",
+    "url": "/static/js/2.caa4fef7.chunk.js"
+  },
+  {
+    "revision": "2e45a5ba8fe58dd91b7522d247ad66a8",
+    "url": "/static/js/2.caa4fef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff842c5436b818d55093",
+    "url": "/static/js/main.a3c06d4a.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.a3c06d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4422e0e3e9ef4c9afc",
+    "url": "/static/js/runtime-main.cc74d943.js"
+  }
+]);
