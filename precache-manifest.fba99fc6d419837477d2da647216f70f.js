9b47459b2a29d8f87ecf10491860d3f563402b1a
Samson Svendsen <Svendsen@nordictrustee.com>
2022-10-05 14:11:17 +0200

    Updates

--- precache-manifest.fba99fc6d419837477d2da647216f70f.js
+++ precache-manifest.fba99fc6d419837477d2da647216f70f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "42aac80797ae01554c3c71c1e1cc6421",
+    "revision": "2e5913d7b548a7ba919b07e968b481e8",
     "url": "/index.html"
   },
   {
-    "revision": "ff0e232abd1a0b31867d",
+    "revision": "f7436634c1ee7708fa85",
     "url": "/static/css/main.69eda509.chunk.css"
   },
   {
@@ -16,12 +16,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.caa4fef7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ff0e232abd1a0b31867d",
-    "url": "/static/js/main.a92773f4.chunk.js"
+    "revision": "f7436634c1ee7708fa85",
+    "url": "/static/js/main.ced83425.chunk.js"
   },
   {
     "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
-    "url": "/static/js/main.a92773f4.chunk.js.LICENSE.txt"
+    "url": "/static/js/main.ced83425.chunk.js.LICENSE.txt"
   },
   {
     "revision": "6f4422e0e3e9ef4c9afc",
